911dae0389c05f51cf0946d7b281226d5373f0b7
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-14 11:37:31 +0000

    live coding

--- js/script.js
+++ js/script.js
@@ -18,19 +18,41 @@ for (let i=0; i < imagesArray.length; i++) {
 // stato di partenza
 const itemsArray = document.getElementsByClassName("item");
 console.log(itemsArray);
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
 
+// Imposto variabile d'appoggio uguale a 0 e dico a js di aggiungere classe active all'elemento dell'array con classe = a variabile d'appoggio
 let activeItemIndex = 0;
 itemsArray[activeItemIndex].classList.add("active");
+prevBtn.classList.add("hidden")
 
 // Gestisco il click sul bottone next
-const nextBtn = document.querySelector(".next");
-console.log(nextBtn);
+
 nextBtn.addEventListener("click", function() {
+    prevBtn.classList.remove("hidden");
     // rimuovere activeIndex dalla slide corrente
     itemsArray[activeItemIndex].classList.remove("active");
     // incremento activeIndex
     activeItemIndex++;
     // aggiungo active a quello nuovo
     itemsArray[activeItemIndex].classList.add("active");
-    // blocco il nextBtn all'ultima slide
+    // faccio scomparire il nextBtn all'ultima slide
+    if (activeItemIndex === itemsArray.length - 1) {
+        nextBtn.classList.add("hidden");
+    }
+})
+
+// Gestisco il click sul bottone prev
+nextBtn.addEventListener("click", function() {
+    nextBtn.classList.remove("hidden");
+
+    if (activeItemIndex === 0) {
+        prevBtn.classList.add("hidden");
+    }
+    // rimuovere activeIndex dalla slide corrente
+    itemsArray[activeItemIndex].classList.remove("active");
+    // incremento activeIndex
+    activeItemIndex--;
+    // aggiungo active a quello nuovo
+    itemsArray[activeItemIndex].classList.add("active");
 })
